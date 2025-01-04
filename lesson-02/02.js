bf45c1e53a0990e4cdb0210c9a3e1b9c75d073dc
dskuzniatsou <sovyclub@gmail.com>
2025-01-04 22:23:45 +0300

    lesson-02/02.js-identified_isWinningDouble

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -13,9 +13,9 @@ let isWinningDouble // your code
 
 dice1 = Math.floor((Math.random() * 6 )+ 1)
 dice2 = Math.floor((Math.random() * 6 )+ 1)
-if ((dice1 === dice2)&&(dice1 > 3)) {
+if ((dice1 === dice2)&&((dice1&&dice2) > 3)) {
   isWinningDouble = true
-}
+} else { isWinningDouble = false}
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
